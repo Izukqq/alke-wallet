cd1b0fb3b89535e5d81ff84b1dba0a082531c798
Kevin <kevinmorillas1@gmail.com>
2026-01-18 21:07:26 -0300

    Implementacion de Jquery de depositos.js

--- Js/deposito.js
+++ Js/deposito.js
@@ -1,33 +1,42 @@
-const inputMonto = document.getElementById('deposito-input');
-const botonDepositar = document.getElementById('deposito-btn');
+$(document).ready(function () {
+  // SELECTORES
+  const $inputMonto = $('#deposito-input');
+  const $botonDepositar = $('#deposito-btn');
 
-let saldoActual = sessionStorage.getItem('miSaldo') || 0;
+  // VARIABLES
+  let saldoActual = sessionStorage.getItem('miSaldo') || 0;
 
-function guardarTransaccionDeposito(monto) {
+  // FUNCIONES
+
+  // Guardar transacción de depósito
+  function guardarTransaccionDeposito(monto) {
     const transacciones = JSON.parse(sessionStorage.getItem('misTransacciones')) || [];
     
     const fecha = new Date();
     const fechaFormato = `${String(fecha.getDate()).padStart(2, '0')}/${String(fecha.getMonth() + 1).padStart(2, '0')}/${fecha.getFullYear()}`;
     
     const transaccion = {
-        fecha: fechaFormato,
-        tipo: 'Depósito',
-        contacto: 'Mi cuenta',
-        monto: monto,
-        timestamp: fecha.getTime()
+      fecha: fechaFormato,
+      tipo: 'Depósito',
+      contacto: 'Mi cuenta',
+      monto: monto,
+      timestamp: fecha.getTime()
     };
     
     transacciones.push(transaccion);
     sessionStorage.setItem('misTransacciones', JSON.stringify(transacciones));
-}
+  }
 
-botonDepositar.addEventListener('click', function() {
-    const montoTexto = inputMonto.value;
+  // EVENTOS
+
+  // Click en botón "Realizar depósito"
+  $botonDepositar.on('click', function() {
+    const montoTexto = $inputMonto.val();
     const montoNumero = parseFloat(montoTexto);
 
     if (isNaN(montoNumero) || montoNumero <= 0) {
-        alert("Por favor, ingresa un monto válido mayor a 0.");
-        return;
+      alert("Por favor, ingresa un monto válido mayor a 0.");
+      return;
     }
 
     const saldoTotal = parseFloat(saldoActual) + montoNumero;
@@ -37,4 +46,5 @@ botonDepositar.addEventListener('click', function() {
 
     alert(`¡Depósito exitoso! Tu nuevo saldo es: ${saldoTotal}`);
     window.location.href = 'menu.html';
+  });
 });
